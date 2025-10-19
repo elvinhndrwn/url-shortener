d1f7ba588e8fdf72c4121748de007d265c21df75
elvinhndrwn <elvin.hendrawan@ist.id>
2025-10-19 12:29:01 +0700

    change nano import

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,23 +1,24 @@
-const { nanoid } = require("nanoid");
 const { saveUrl, findUrl } = require("../models/urlModel");
 
 const shortenUrl = async (req, res) => {
-  const { originalUrl } = req.body;
+  if (!req.body.originalUrl)
+    return res.status(400).json({ message: "URL is required" });
 
-  if (!originalUrl) return res.status(400).json({ message: "URL is required" });
-
+  // Dynamic import nanoid
+  const { nanoid } = await import("nanoid");
   const shortCode = nanoid(6);
 
-  const newData = await saveUrl(shortCode, originalUrl);
+  const newData = await saveUrl(shortCode, req.body.originalUrl);
 
   res.json({
-    originalUrl,
+    originalUrl: req.body.originalUrl,
     shortUrl: `${process.env.BASE_URL}/${shortCode}`,
   });
 };
 
 const redirectUrl = async (req, res) => {
   const { shortCode } = req.params;
+
   const data = await findUrl(shortCode);
 
   if (!data) return res.status(404).send("URL not found");
