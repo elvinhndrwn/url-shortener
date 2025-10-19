a5ee92fe8c9a2291204b7c52e4437c82476ccf0d
elvinhndrwn <elvin.hendrawan@ist.id>
2025-10-19 12:58:58 +0700

    fix

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -8,7 +8,7 @@ export const shortenUrl = async (req, res) => {
       return res.status(400).json({ message: "URL is required" });
 
     const shortCode = nanoid(6);
-    const newData = await saveUrl(shortCode, req.body.originalUrl);
+    await saveUrl(shortCode, req.body.originalUrl);
 
     res.json({
       originalUrl: req.body.originalUrl,
@@ -25,7 +25,9 @@ export const redirectUrl = async (req, res) => {
   try {
     const { shortCode } = req.params;
     const data = await findUrl(shortCode);
+
     if (!data) return res.status(404).send("URL not found");
+
     res.redirect(data.original_url);
   } catch (error) {
     console.error("Error in redirectUrl:", error);
